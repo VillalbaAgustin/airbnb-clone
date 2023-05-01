8a7a9a30836654518a7541f8b439d2a76e371e02
Agustín Villalba <villalba.agus.99@gmail.com>
2023-05-01 00:22:36 -0300

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import { IndexPage } from "./pages/IndexPage";
+import { LoginPage } from "./pages/LoginPage";
+import { Layout } from "./layout/Layout";
+import { RegisterPage } from "./pages/RegisterPage";
+
+export const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<IndexPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Route>
+    </Routes>
+  );
+};
