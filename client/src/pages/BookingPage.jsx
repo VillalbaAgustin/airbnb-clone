f5aa8b42485187d1c90e26b50c73624ba236ef7f
Villalba Agustin <villalba.agus.99@gmail.com>
2023-06-29 13:09:36 -0300

    BookingsPage component updated

--- client/src/pages/BookingPage.jsx
+++ client/src/pages/BookingPage.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 
 export const BookingPage = () => {
   const {id} = useParams();
+  const [booking, setBooking] = useState(null)
+  
+  useEffect(() => {
+    axios.get('/bookings').then(response => {
+      const foundBooking = response.data.find(({_id}) => _id === id);
+      if (foundBooking) {
+        setBooking(foundBooking);
+      }
+    })
+  }, [id]);
+  
+  if (!booking) {
+    return '';
+  }
 
   return (
     <div>single BookingPage: {id}</div>
