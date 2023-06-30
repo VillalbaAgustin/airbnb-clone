d9a29cda27a47176b639b9e6161bffffbebda0c8
VillalbaAgustin <villalba.agus.99@gmail.com>
2023-06-30 15:51:21 -0300

    refactor PlacePage Componente

--- client/src/pages/BookingPage.jsx
+++ client/src/pages/BookingPage.jsx
@@ -1,6 +1,9 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
+import { AddressLink } from '../components/AddressLink';
+import { PlaceGallery } from '../components/PlaceGallery';
+import { BookingDates } from '../components/BookingDates';
 
 export const BookingPage = () => {
   const {id} = useParams();
@@ -20,6 +23,20 @@ export const BookingPage = () => {
   }
 
   return (
-    <div>single BookingPage: {id}</div>
+    <div className='my-8'>
+      <h1 className="text-3xl">{booking.place?.title}</h1>
+      <AddressLink className='my-2 block'>{booking.place.address}</AddressLink>
+      <div className="bg-gray-200 p-2 my-5 rounded-2xl flex items-center justify-between">
+        <div>
+          <h2 className='text-xl mb-2'>Your booking information:</h2>
+          <BookingDates booking={booking}/>
+        </div>
+        <div className='bg-primary p-3 text-white rounded-2xl'>
+          <div>Total price:</div>
+          <div className='text-2xl'>${booking.price}</div>
+        </div>
+      </div>
+      <PlaceGallery place={booking.place}/>
+    </div>
   )
 }
